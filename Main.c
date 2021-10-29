e4e8587e33e5d73b577fbe5d05009f14a83e6652
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2021-10-30 00:54:02 +0530

    initial commit

---
+++ Main.c
@@ -0,0 +1,21 @@
+/*  Project Title: Spell Checker Project
+    Author: Sahil Afrid Farookhi
+    Programming language: C
+    Concepts used: Files, loops, control statements, and variable declaration
+    File name: dictionary.txt   */
+
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#include <process.h>
+#include <stdlib.h>
+
+#include "Add.c"
+
+int add(int, int);
+
+void main()
+{
+    int result = add(1, 2);
+    printf("The result is %d.", result);
+}
---
+++ Test.c
@@ -0,0 +1,8 @@
+#include "Add.c"
+
+int add(int, int);
+
+void main()
+{
+    int result = add(1, 2);
+}
