706f5874707d217716d6abcd18cca012e140d389
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2021-10-31 15:38:15 +0530

    UnitTest.c implemented and tested

--- Test.c
+++ Test.c
@@ -1,8 +1,27 @@
 #include "Add.c"
+#include "UnitTest.c"
 
 int add(int, int);
+void numberEquals(double number1, double number2);
 
 void main()
 {
+    int i;
     int result = add(1, 2);
+    // numberEquals(result, 3);
+    // numberEquals(2.5, 3.5);
+    int array1[3], array2[3];
+
+    array1[0] = 1;
+    array1[1] = 2;
+    array1[2] = 3;
+
+    array2[0] = 1;
+    array2[1] = 2;
+    array2[2] = 3;
+
+    for (i = 0; i < 3; i++)
+    {
+        numberEquals(array1[i], array2[i]);
+    }
 }
---
+++ UnitTest.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+void numberEquals(double number1, double number2)
+{
+    int passed = 0, failed = 0, total = 0;
+    if (!(isdigit(number1) && isdigit(number2)))
+    {
+        if (number1 == number2)
+        {
+            passed++;
+        }
+        else
+        {
+            failed++;
+        }
+        total = passed + failed;
+    }
+    else
+    {
+        printf("Error: %d", errno);
+        exit(EXIT_FAILURE);
+    }
+    if (passed > 0)
+    {
+        printf("%d of %d test cases passed.\n", passed, total);
+    }
+    if (failed > 0)
+    {
+        printf("%d of %d test cases failed.\n", failed, total);
+    }
+    printf("Total test cases %d.\n", total);
+}
